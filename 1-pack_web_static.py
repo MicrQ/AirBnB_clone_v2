81c3a7d0154718bfba1bd476114a6c32b60e8ce6
MicrQ <mail.micrq@proton.me>
2024-04-07 06:41:41 +0300

    pycodestyle: a python script that create a .tgz file of web_static.

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
-""" a python script that create a .tgz file of web_static. """
+""" """
 
 import os
 from fabric import api
 from datetime import datetime
+
 
 def do_pack():
     """ a function that create .tgz of
@@ -12,4 +13,4 @@ def do_pack():
     if not os.path.exists('versions'):
         os.mkdir('versions')
     date = datetime.now().strftime("%Y%m%d%H%M%S")
-    api.local('tar -czvf versions/web_static_{}.tgz web_static'.format(str(date)))
+    api.local('tar -czvf versions/web_static_{}.tgz web_static'.format(date))
