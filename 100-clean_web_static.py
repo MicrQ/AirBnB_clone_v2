c986ade30dec7b05c4c54e62e8201cb6c6615b89
MicrQ <mail.micrq@proton.me>
2024-04-08 18:13:19 +0300

    do_clean function modified

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -3,24 +3,26 @@
     excluding the most recent {number} files.
     where {number} is files to be kept.
 """
+import os
 from fabric import api
-from os import listdir
-
 
 api.env.hosts = ['18.204.8.72', '54.160.83.215']
 
 
 def do_clean(number=0):
-    """ a function that deletes deletes old version archives """
-    if number == 0:
+    """ a function that deletes unnecessary archives """
+    if int(number) == 0:
         number = 1
-    archives = sorted(listdir('versions'), reverse=True)
+    else:
+        number = int(number)
 
-    with api.lcd('versions'):
-        for i in range(int(number), len(archives)):
-            api.local('rm {}'.format(archives[i]))
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with api.lcd("versions"):
+        [api.local("rm ./{}".format(a)) for a in archives]
 
-    with api.cd('/data/web_static/releases'):
-        archives = sorted(api.run('ls').split(), reverse=True)
-        for i in range(int(number), len(archives)):
-            api.run('sudo rm -rf {}'.format(archives[i]))
+    with api.cd("/data/web_static/releases"):
+        archives = api.run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [api.run("rm -rf ./{}".format(a)) for a in archives]
