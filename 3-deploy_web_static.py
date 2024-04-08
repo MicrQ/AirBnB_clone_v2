59ca34d550f1973eaef0e06e65a9cf6ba2fdabe9
MicrQ <mail.micrq@proton.me>
2024-04-08 05:58:22 +0300

    last modification

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -14,10 +14,9 @@ def do_pack():
     """ a function that create .tgz of
         web_static in the folder named versions
     """
-    if not os.path.exists('versions'):
-        os.mkdir('versions')
+    api.local('sudo mkdir -p versions')
     date = datetime.now().strftime("%Y%m%d%H%M%S")
-    result = api.local('tar -czvf versions/web_static_{}.tgz \
+    result = api.local('sudo tar -czvf versions/web_static_{}.tgz \
                        web_static'.format(date))
     if result.succeeded:
         return "versions/web_static_{}.tgz".format(date)
@@ -26,24 +25,24 @@ def do_pack():
 
 def do_deploy(archive_path):
     """ a function that distributes the archive """
-    if not os.path.exists(archive_path):
+    if os.path.exists(archive_path) is False:
         return False
     try:
-        api.put(archive_path, '/tmp/', True)
+        api.put(archive_path, '/tmp/')
         filename = archive_path.split('/')[-1]
 
-        api.sudo('mkdir -p /data/web_static/releases/{}'.format(filename[:-4]))
-        api.sudo('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(
+        api.run('mkdir -p /data/web_static/releases/{}'.format(filename[:-4]))
+        api.run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format(
             filename, filename[:-4]
         ))
-        api.sudo('rm /tmp/{}'.format(filename))
-        api.sudo('rm -fr /data/web_static/current')
-        api.sudo('mv /data/web_static/releases/{}/web_static/* \
+        api.run('rm /tmp/{}'.format(filename))
+        api.run('mv /data/web_static/releases/{}/web_static/* \
                 /data/web_static/releases/{}/'.format(
                     filename[:-4], filename[:-4]))
         api.sudo('rm -fr /data/web_static/releases/{}/web_static'.format(
             filename[:-4]))
-        api.sudo('ln -fs /data/web_static/releases/{} \
+        api.run('rm -fr /data/web_static/current')
+        api.sudo('ln -s /data/web_static/releases/{}/ \
                 /data/web_static/current'.format(filename[:-4]))
         return True
     except:
@@ -53,6 +52,6 @@ def do_deploy(archive_path):
 def deploy():
     """ a function that initiates the deployment"""
     pack = do_pack()
-    if pack is None:
-        return False
-    return do_deploy(pack)
+    if os.path.exists(pack):
+        return do_deploy(pack)
+    return False
